cecb85e49f676332f0f9c1369b6137208663a8a0
FLemon <jinfengxie1986@gmail.com>
2021-04-05 11:49:36 +0100

    add images to categories and remove size-guide from product

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
         database: process.env.DATABASE_NAME || 'strapi',
         username: process.env.DATABASE_USERNAME || 'strapi',
         password: process.env.DATABASE_PASSWORD || 'strapi',
-        ssl: { "rejectUnauthorized": false },
+        ssl: false //{ "rejectUnauthorized": false },
       },
       options: {}
     },
