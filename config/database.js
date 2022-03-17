5607504e3e35a64f5d463b193bde86cfcc612f41
FLemon <jinfengxie1986@gmail.com>
2022-03-17 01:15:12 +0000

    try add in pool config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,16 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false // <- default is true, set to false
+    },
     default: {
       connector: 'bookshelf',
       settings: {
